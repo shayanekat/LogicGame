33f34abc0345ea5b0d70925f04f8bf2e00d359f0
shayanekat <shaykatfsx@gmail.com>
2020-05-24 19:25:20 +0200

    added and finished button block

--- main.py
+++ main.py
@@ -8,21 +8,11 @@ import time
 # - add mouse binding functions 
 # - add others block
 
-# ====Backend====
+# =========================BACKEND=========================
 #init var
-WindowW, WindowH = 500, 500
-S = 50
-selection = ""
-
-#global functions:
-def MenuGrid():
-    levers = []
-    """Fonction qui initialise l'affichage de Menu pour afficher tout les blocks utilisables"""
-    l0 = Lever()
-    l0.id = menu.create_rectangle(l0.x, l0.y, l0.x+S, l0.y+S, fill=l0.color, outline="black")
-    l0.idt = menu.create_text(l0.x+int(S//2), l0.y+int(S//2), text=l0.text, font=("Calibri", "12"), fill="white")
-    levers.append(l0)
-    return(levers)
+WindowW, WindowH = 500, 500     # taille canvas (pixel)
+S = 50                          # taille block
+selection = ""                  # variable pour savoir block selected
 
 # POO
 class Block():
@@ -40,6 +30,11 @@ class Block():
         self.id = 0
         self.idt = 0
 
+    def Place(self):
+        """ Methode qui sert a placer le block dans la fenetre. Va encore changer car doit inclure le click de souris"""
+        self.id = main.create_rectangle(self.x, self.y, self.x+S, self.y+S, fill=self.color, outline="black")
+        self.idt = main.create_text(self.x+int(S//2), self.y+int(S//2), text=self.text, font=("Calibri", "12"), fill="white")
+
 class Lever(Block):
     """Class Levier qui descent de Block \n
      - input
@@ -50,11 +45,6 @@ class Lever(Block):
         Block.__init__(self, x, y, text, color)
         self.state = 0
         self.output = [[self.x+int(S//2), self.y]]
-    
-    def Place(self):
-        """ Methode qui sert a placer le block dans la fenetre. Va encore changer car doit inclure le click de souris"""
-        self.id = main.create_rectangle(self.x, self.y, self.x+S, self.y+S, fill=self.color, outline="black")
-        self.idt = main.create_text(self.x+int(S//2), self.y+int(S//2), text=self.text, font=("Calibri", "12"), fill="white")
 
     def ChangeState(self):
         """ Methode principale : le changement d'etat du levier entre 0 et 1. va encore changer """
@@ -68,17 +58,74 @@ class Lever(Block):
         main.itemconfig(self.id, fill=self.color)
 
 
+class Boutton(Block):
+    """Class Bouton qui descent de Block \n
+     - input
+     - etat 0 : la sortie est a 0
+     - etat 1 : la sortie est a 1 pendant une seconde
+     - la sortie repasse à 0
+     """
+    def __init__(self, x=0, y=0, text="boutton", color="black"):
+        Block.__init__(self, x, y, text, color)
+        self.state = 0
+        self.output = [[self.x+int(S//2), self.y]]
+
+    def Use(self):
+        """ Methode principale : le boutton passe a 1 pendant une seconde """
+        print("button used")
+        self.state = 1
+        self.color="white"
+        main.itemconfig(self.idt, fill="black")
+        main.itemconfig(self.id, fill=self.color)
+        main.update()
+
+        time.sleep(1)
+
+        self.state = 0
+        self.color="black"
+        main.itemconfig(self.idt, fill="white")
+        main.itemconfig(self.id, fill=self.color)
+        main.update()
+
+
 # fonctions globales
+def MenuGrid():
+    """Fonction qui initialise l'affichage de Menu pour afficher tout les blocks utilisables"""
+    levers = []
+    bouttons = []
+
+    l0 = Lever()
+    b0 = Boutton(x = S)
+
+    l0.id = menu.create_rectangle(l0.x, l0.y, l0.x+S, l0.y+S, fill=l0.color, outline="black")
+    l0.idt = menu.create_text(l0.x+int(S//2), l0.y+int(S//2), text=l0.text, font=("Calibri", "12"), fill="white")
+    b0.id = menu.create_rectangle(b0.x, b0.y, b0.x+S, b0.y+S, fill=b0.color, outline="black")
+    b0.idt = menu.create_text(b0.x+int(S//2), b0.y+int(S//2), text=b0.text, font=("Calibri", "12"), fill="white")
+
+    levers.append(l0)
+    bouttons.append(b0)
+
+    return(levers, bouttons)
+
+
 def test():
     """ Fonction temporaire de bouton pour tester le code"""
     pass
 
+
 def menuMLC(event):
     """Fonction de click gauche de souris dans le menu : selection du block"""
     global selection
-    if event.x // S == 0 and event.y // S == 0:
-        selection = "lever"
-        menu.itemconfig(levers[0].id, outline="blue", width=3)
+    unselect()
+    if event.y // S == 0:
+        if event.x // S == 0 :
+            selection = "lever"
+            menu.itemconfig(levers[0].id, outline="blue", width=3)
+
+        elif event.x // S == 1:
+            selection = "boutton"
+            menu.itemconfig(bouttons[0].id, outline="blue", width=3)
+
 
 def mainMLC(event):
     """ Fonction de click gauche de souris dans main : placer block selectionné | selection pour le wire"""
@@ -86,15 +133,34 @@ def mainMLC(event):
         l = Lever(x=event.x-S//2,y=event.y-S//2)
         l.Place()
         levers.append(l)
+    
+    elif selection == "boutton":
+        btn = Boutton(x=event.x-S//2,y=event.y-S//2)
+        btn.Place()
+        bouttons.append(btn)
+
 
 def mainMRC(event):
     """ Fonction de click droit de souris dans main : utiliser l'input """
     for l in levers:
         if l.x <= event.x <= l.x+S and l.y <= event.y <= l.y+S:
             l.ChangeState()
-            # break
+            break
+    
+    for btn in bouttons:
+        if btn.x <= event.x <= btn.x+S and btn.y <= event.y <= btn.y+S:
+            btn.Use()
+            break
 
-# ====frontend====
+
+def unselect():
+    if selection == "lever":
+        menu.itemconfig(levers[0].id, outline="black", width=1)
+    elif selection == "boutton":
+        menu.itemconfig(bouttons[0].id, outline="black", width=1)
+
+
+# =========================FRONTEND=========================
 root = Tk()
 root.title("LogicGame by Shayane Katchera")
 
@@ -109,10 +175,11 @@ menu.bind("<Button-1>", menuMLC)
 menu.pack(padx=5, pady=5)
 
 # init
-levers = MenuGrid()
+levers, bouttons = MenuGrid()
 
 # test
 b = Button(root,text="test",command=test)
 b.pack()
+
 
 root.mainloop()
